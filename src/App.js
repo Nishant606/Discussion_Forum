62efd8fd147fafda6f766f1c6db665250bf17553
Tejas Bhavsar <tbhavsar121@gmail.com>
2021-05-03 22:19:52 +0530

    added some basic css

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import Post from './posts'
 
 
 function App() {
-  const [post, setPost] = useState([])
-  const [newPost, setnewPost] = useState('')
-  const [text, setText] = useState('')
+  const [post, setPost] = useState([]);
+  const [newPost, setnewPost] = useState('');
+  const [text, setText] = useState();
   useEffect(() =>{
   },[newPost]);
 
   const storePost = e=>{
     setnewPost(text);
     e.preventDefault();
-    setPost([...post, text]);
+    setPost([text,...post]);
     setText('');
   }
 
@@ -24,9 +24,9 @@ function App() {
 
   return (
     <div className="App">
-      <form onSubmit={storePost}>
-        <input type="text" value={text} onChange={content}></input>
-        <button type="submit" value="Post">Post</button>
+      <form className="input-form" onSubmit={storePost}>
+        <input className="input-title" placeholder="Topic of Discussion" type="text" value={text} onChange={content}></input>
+        <button className="input-button" type="submit" value="Post">Post</button>
       </form>
       {post.map(post => (
         <Post title= {post}></Post>
